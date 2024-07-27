5cdb287cd44c4f6c21c26f9868cdf962671d6c5d
Filip-Povidernyi <p.poviderniy@gmail.com>
2024-07-27 11:11:52 +0300

    gf

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,9 +1,13 @@
 import Joi from "joi";
 
 export const createContactSchema = Joi.object({
-
+    name: Joi.string().alphanum().min(1).max(30).required(),
+    email: Joi.string().email().required(),
+    phone: Joi.string().min(7).required(),
 });
 
 export const updateContactSchema = Joi.object({
-
+    name: Joi.string().alphanum().min(1).max(30),
+    email: Joi.string().email(),
+    phone: Joi.string().min(7),
 });
--- services/contactsServices.js
+++ services/contactsServices.js
@@ -1,9 +1,109 @@
-import Joi from "joi";
+import fs from "node:fs/promises";
+import path from "node:path";
+import { nanoid } from "nanoid";
 
-export const createContactSchema = Joi.object({
 
-});
 
-export const updateContactSchema = Joi.object({
+const contactsPath = path.resolve("db", "contacts.json");
 
-});
+const contacts = async () => {
+
+    const data = await fs.readFile(contactsPath, "utf-8");
+
+    return JSON.parse(data);
+};
+
+
+export async function listContacts() {
+
+    const contactsList = await contacts();
+
+    return contactsList;
+};
+
+export async function getContactById(contactId) {
+
+    const contactsList = await contacts();
+    const contact = contactsList.find(contact => contact.id === contactId);
+
+    return contact || null;
+};
+
+export async function removeContact(contactId) {
+
+    if (contactId.length !== 21) {
+        console.warn(`\x1B[31m Invalid id: ${contactId}`);
+
+        return null;
+    }
+
+    const contactsList = await contacts();
+
+    const index = contactsList.findIndex(contact => contact.id === contactId);
+
+    if (index === -1) {
+        console.log(`\x1B[31m No contact with this id: ${contactId}`);
+
+        return null;
+    };
+
+    const remContact = contactsList.splice(index, 1);
+
+    await fs.writeFile(contactsPath, JSON.stringify(contactsList, null, 2));
+
+    return remContact[0];
+};
+
+export async function addContact(data) {
+
+    const contactsList = await contacts();
+
+    const newContact = {
+        id: nanoid(),
+        ...data,
+    };
+
+    contactsList.push(newContact);
+
+    await fs.writeFile(contactsPath, JSON.stringify(contactsList, null, 2));
+
+    return newContact;
+
+};
+
+export async function updateContacts(id, data) {
+    const { name, email, phone } = data;
+    const contactsList = await contacts();
+    const contact = await getContactById(id);
+    console.log('contact', contact)
+    if (!contact) {
+        return null;
+    }
+    if (name) {
+        contact.name = name;
+    };
+    if (email) {
+        contact.email = email;
+    };
+    if (phone) {
+        contact.phone = phone;
+    };
+
+    contactsList.push(contact);
+
+    await fs.writeFile(contactsPath, JSON.stringify(contactsList, null, 2));
+
+    await removeContact(id);
+    return contact;
+};
+
+
+const contactsService = {
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact,
+    updateContacts,
+};
+
+export default contactsService
