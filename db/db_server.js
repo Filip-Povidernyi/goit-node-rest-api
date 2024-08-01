831cf9c9f65c1243b6283e9aa6c244f448d7f42a
Filip <137682962+Filip-Povidernyi@users.noreply.github.com>
2024-08-01 12:50:27 +0300

    dotenv
    add dotenv configs

--- db/db_server.js
+++ db/db_server.js
@@ -1,19 +1,16 @@
-import dotenv from 'dotenv';
 import { Sequelize } from 'sequelize';
 
 
-
-dotenv.config();
-const DB_PASS = process.env.DB_PASSWORD;
+const {DIALECT, USERNAME, DATABASE, PASSWORD, HOST, PORT, PROTOCOL} = process.env();
 
 export const sequelize = new Sequelize({
-    dialect: 'postgres',
-    username: "filip_povidernyi",
-    database: "db_contacts_gjuf",
-    password: "82rfF0iHMVvWfUbQx6uvKZGyT1ti60X6",
-    host: "dpg-cqjmsf5ds78s73f2bgug-a.frankfurt-postgres.render.com",
-    port: "5432",
-    protocol: 'postgres',
+    dialect: DIALECT,
+    username: USERNAME,
+    database: DATABASE,
+    password: PASSWORD,
+    host: HOST,
+    port: PORT,
+    protocol: PROTOCOL,
     dialectOptions: {
         ssl: true,
     }
