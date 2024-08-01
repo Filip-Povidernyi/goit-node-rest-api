cb74be1f294f53ca3afaa154d6c7532fb3726a16
Filip <137682962+Filip-Povidernyi@users.noreply.github.com>
2024-08-01 12:15:18 +0300

    Update db_server.js

--- db/db_server.js
+++ db/db_server.js
@@ -10,7 +10,7 @@ export const sequelize = new Sequelize({
     dialect: 'postgres',
     username: "filip_povidernyi",
     database: "db_contacts_gjuf",
-    password: `${DB_PASS}`,
+    password: "82rfF0iHMVvWfUbQx6uvKZGyT1ti60X6",
     host: "dpg-cqjmsf5ds78s73f2bgug-a.frankfurt-postgres.render.com",
     port: "5432",
     protocol: 'postgres',
